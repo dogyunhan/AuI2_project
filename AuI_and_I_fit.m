a4e3c2083b33ecf0be04ac209f883c2ce63338c6
dogyunhan <handogyun02@gmail.com>
2025-12-31 10:53:22 +0900

    check

--- AuI_and_I_fit.m
+++ AuI_and_I_fit.m
@@ -12,14 +12,17 @@ elem_prod = [53, 79, 53];      % Product State: Au-I (Diatomic)
 base_path = "\\172.30.150.180\homes\sdlab\230425_ESRF_AuBr2\SCRIPTS\inHouseProcess\resultsCD";
 files = struct();
 files.solv     = fullfile(base_path, "heating_MeCN_0001", "merged_solv_dat.dat");
-files.sads     = fullfile(base_path, "AuI2_30mM_0002", "SADS_comps_3.dat"); 
-files.sads_std = fullfile(base_path, "AuI2_30mM_0002", "std_SADS_comps_3.dat"); 
+files.sads     = fullfile(base_path, "AuI2_30mM_0002", "SADS_comps_4.dat"); 
+files.sads_std = fullfile(base_path, "AuI2_30mM_0002", "std_SADS_comps_4.dat"); 
+
+target_SADS = 4;
+title = 'r_{Au-I} = %.4f, %.4f, theta = %.4f, r_{GS}1 = %.4f, r_{GS}2 = %.4f, theta = %.4f';
 
 % [Fitting Parameters]
 fit_range = [1.0, 7.0];    % q Fitting Range (A^-1)
-init_pars = horzcat([2.0 2.0 150], [2.505 2.505 180]); 
-lb        = horzcat([2.0 2.0 95], [2.505 2.505 180]);  % lower bound
-ub        = horzcat([3.0 3.0 180], [2.505 2.505 180]);  % upper bound
+init_pars = horzcat([2.0 2.0 150], [2.4 2.4 180]); 
+lb        = horzcat([2.0 2.0 90], [2.4 2.4 180]);  % lower bound
+ub        = horzcat([3.0 3.0 180], [2.8 2.8 180]);  % upper bound
 
 % [External Script] 상수 로드
 run atom_consts.m % xfactor 로드
@@ -39,8 +42,8 @@ q_full = raw_dat(:, 1);
 mask   = (q_full > fit_range(1)) & (q_full < fit_range(2));
 
 q_fit = q_full(mask);         % Fitting용 q 벡터
-sads_comp = raw_dat(mask, 3);    % idx=2이면 comp는 1
-std_comp = raw_std(mask, 3);     
+sads_comp = raw_dat(mask, target_SADS+1);    % idx=2이면 comp는 1
+std_comp = raw_std(mask, target_SADS+1);     
 
 % 2.3. Solvent Heating Data Processing
 % 용매 데이터도 동일한 q grid를 갖는다고 가정하고 같은 mask 적용
@@ -106,7 +109,7 @@ plot_data(2).y = out.fit_dSq;
 plot_data(2).color = 'blue'; 
 plot_data(2).label = 'Theory Fit';
 
-plot_title = sprintf('Fit Result: r_{Au-I} = %.4f A, r_{GS}1 = %.4f, r_{GS}2 = %.4f, theta = %.4f', out.params);
+plot_title = sprintf(title, out.params);
 
 DHanfuncs.custom_plot(plot_data, LineWidth=1.5, Title=plot_title, XLim=fit_range);
 
