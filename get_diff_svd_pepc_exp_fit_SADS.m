d47e20e24394efac6398d39e3cb80d18fa000368
dogyunhan <handogyun02@gmail.com>
2025-12-31 10:31:39 +0900

    check

--- get_diff_svd_pepc_exp_fit_SADS.m
+++ get_diff_svd_pepc_exp_fit_SADS.m
@@ -42,8 +42,8 @@ tds_all = [-3e-9, 0, ...
 % verbose = true;
 verbose = false;
 
-% save = true;
-save = false;
+save = true;
+% save = false;
 
 [mergedData, mergedStd, ~, ~, q] = process_trxl_runs(default_path, runs, file_path, tds_all, [4, 7], true);
 if save
@@ -246,7 +246,7 @@ time_constants = xl([10 14 18 19]);
 [SAC, std_SAC, theory_profile, profile_SAC, std_profile_SAC] = HKifuncs.KCA(DataPEPCed, mergedStd(qPEPC, :), q(qPEPC), tds_merge, -1, time_constants, tds_merge, 111, [1, 1e6]);
 % [DADS, std_DADS, theory_profile_d, profile_SAC_d, std_profile_SAC_d] = HKifuncs.KCA_DADS(data_merge_all, std_merge_all, q, tds_merge, -1, time_constants, tds_merge, 200, [1 1e6]);
 %%
-SADS_comps = 3;
+SADS_comps = 4;
 recon = SAC(:, 1:SADS_comps) * profile_SAC(:, 1:SADS_comps)';
 
 HKifuncs.draw_contour(DataPEPCed, tds_all(2:end)*1e12, q_pc, 666, "PEPCed data", [], [-3e-4 3e-4])
@@ -257,9 +257,9 @@ HKifuncs.draw_contour(DataPEPCed - recon, tds_all(2:end)*1e12, q_pc, 888, sprint
 
 if save
     writematrix([q(qPEPC) SAC(:, 1:SADS_comps)], fullfile(default_path, sprintf("SADS_comps_%d.dat", SADS_comps)));
-    writematrix([q(qPEPC) std_SAC(:, 1:SADS_comps)], fullfile(default_path, sprintf("./std_SADS_comps_%d.dat", SADS_comps)));
+    writematrix([q(qPEPC) std_SAC(:, 1:SADS_comps)], fullfile(default_path, sprintf("std_SADS_comps_%d.dat", SADS_comps)));
     % writematrix([q(qPEPC) DADS(:, 1:DADS_comps)], fullfile(default_path, sprintf("DADS_comps_%d.dat", SADS_comps)));
-    % writematrix([q(qPEPC) std_DADS(:, 1:DADS_comps)], fullfile(default_path, sprintf("./std_DADS_comps_%d.dat", SADS_comps)));
+    % writematrix([q(qPEPC) std_DADS(:, 1:DADS_comps)], fullfile(default_path, sprintf("std_DADS_comps_%d.dat", SADS_comps)));
 end
 
 fprintf('%.6e ', xl(ind));
