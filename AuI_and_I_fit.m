f894b89592c0a8c06227154f0b8e877625f40f1d
dogyunhan <handogyun02@gmail.com>
2026-01-02 17:28:38 +0900

    param 수정

--- AuI_and_I_fit.m
+++ AuI_and_I_fit.m
@@ -17,7 +17,7 @@ files.solv     = fullfile(base_path, "heating_MeCN_0001", "merged_solv_dat.dat")
 files.sads     = fullfile(base_path, "AuI2_30mM_0002", "SADS_comps_4.dat"); 
 files.sads_std = fullfile(base_path, "AuI2_30mM_0002", "std_SADS_comps_4.dat"); 
 
-target_SADS = 4;
+target_SADS = 2;
 title = ['r_{iso} = %.4f & %.4f, theta_{iso} = %.4f ' ...
     'r_{AuI} = %.4f ' ...
     'r_{GS} = %.4f & %.4f, theta_{GS} = %.4f ', ...
@@ -25,9 +25,9 @@ title = ['r_{iso} = %.4f & %.4f, theta_{iso} = %.4f ' ...
 
 % [Fitting Parameters]
 fit_range = [1.0, 7.0];    % q Fitting Range (A^-1)
-init_pars = horzcat([2.3, 2.3, 165], 2.56, [2.5160 2.4886 180], 1); % bent, AuI, GS
-lb        = horzcat([2.0, 2.0, 90], 2.0, [2.5160 2.4886 180], 0);  % lower bound
-ub        = horzcat([3.0, 3.0, 180], 3.0, [2.5160 2.4886 180], 2);  % upper bound
+init_pars = horzcat([2.3, 2.3, 165], 2.5609, [2.5160 2.4886 180], 1); % bent, AuI, GS
+lb        = horzcat([2.0, 2.0, 90], 2.5609, [2.5160 2.4886 180], 0);  % lower bound
+ub        = horzcat([3.0, 3.0, 180], 2.5609, [2.5160 2.4886 180], 2);  % upper bound
 
 % [External Script] 상수 로드
 run atom_consts.m % xfactor 로드
