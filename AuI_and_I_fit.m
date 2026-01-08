3d560e6d7cb1b4b3a105667b606268c7f068c9a4
dogyunhan <handogyun02@gmail.com>
2026-01-08 22:25:11 +0900

    d

--- AuI_and_I_fit.m
+++ AuI_and_I_fit.m
@@ -18,7 +18,7 @@ files.solv     = fullfile(base_path, "heating_MeCN_0001", "merged_solv_dat.dat")
 files.dads     = fullfile(base_path, "AuI2_30mM_0002", "DADS_comps_4.dat"); 
 files.dads_std = fullfile(base_path, "AuI2_30mM_0002", "std_DADS_comps_4.dat"); 
 
-target_DADS = 1;
+target_DADS = 2;
 title = 'r_{I2} = %.4f / r_{AuI} = %.4f';
 
 % [Fitting Parameters]
