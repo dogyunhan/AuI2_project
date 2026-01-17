00829e05ed047e4110a6fef2d0ad319d6fef498d
dogyunhan <handogyun02@gmail.com>
2026-01-17 20:19:44 +0900

    test

--- AuI_and_I_fit.m
+++ AuI_and_I_fit.m
@@ -15,7 +15,7 @@ files.solv     = fullfile(base_path, "heating_MeCN_0001", "merged_solv_dat.dat")
 files.dads     = fullfile(base_path, "AuI2_30mM_0002", "DADS_comps_4.dat"); 
 files.dads_std = fullfile(base_path, "AuI2_30mM_0002", "std_DADS_comps_4.dat"); 
 
-target_DADS = 3;
+target_DADS = 4;
 
 title = ['r_{AuI} = %.4f / ' ...
 'r_{AuI2 dimer} = %.4f, %.4f, %.4f, theta = %.4f '];
@@ -24,9 +24,9 @@ chi_red = true;
 
 % [Fitting Parameters]
 fit_range = [3.0, 7.0];    % q Fitting Range (A^-1)
-init_pars = horzcat(2.5, [2.5 2.5 2.5 120]); 
 lb        = horzcat(2.5, [2.3 2.5 2.5 90]);  % lower bound
-ub        = horzcat(3.0, [3.4 3.4 3.4 180]);  % upper bound
+ub        = horzcat(2.7, [3.4 3.4 3.4 180]);  % upper bound
+init_pars = lb;
 
 % [External Script] 상수 로드
 run atom_consts.m % xfactor 로드
