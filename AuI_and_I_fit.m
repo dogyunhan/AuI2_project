a4acd028f6cc43c1f6b675f6cbca0a8f1e9f602c
dogyunhan <handogyun02@gmail.com>
2026-01-19 10:20:40 +0900

    AuI to Au + I2는 DADS3에 비슷

--- AuI_and_I_fit.m
+++ AuI_and_I_fit.m
@@ -19,15 +19,15 @@ files.dads     = fullfile(base_path, "AuI2_30mM_0002", "DADS_comps_4.dat");
 files.dads_std = fullfile(base_path, "AuI2_30mM_0002", "std_DADS_comps_4.dat"); 
 
 target_DADS = 4;
-title = 'r_{I2} = %.4f / r_{AuI} = %.4f';
+title = 'r_{I2} = %.4f / r_{AuI} = %.4f / chi = %.5f';
 
 chi_red = true;
 
 % [Fitting Parameters]
 fit_range = [3.0, 7.0];    % q Fitting Range (A^-1)
-init_pars = horzcat(2.5, 2.5661); 
-lb        = horzcat(2.4, 2.4);  % lower bound
-ub        = horzcat(2.8, 2.8);  % upper bound
+lb = horzcat(2.0, 2.55);  % lower bound
+ub = horzcat(3.3, 2.65);  % upper bound
+init_pars = lb;
 
 % [External Script] 상수 로드
 run atom_consts.m % xfactor 로드
@@ -123,7 +123,7 @@ plot_data(2).y = out.fit_dSq;
 plot_data(2).color = 'blue'; 
 plot_data(2).label = 'Theory Fit';
 
-plot_title = sprintf(title, out.params);
+plot_title = sprintf(title, out.params, out.chi2);
 
 DHanfuncs.custom_plot(plot_data, LineWidth=1.5, Title=plot_title, XLim=[1 7]);
 
